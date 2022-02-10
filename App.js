1eb4874500bdcce3f8483bb849b5021b1d48156d
alexanderolvera <alexander.olvera943@gmail.com>
2022-02-09 19:52:02 -0600

    Installed stack navigator and added the navigation container to App.js

--- App.js
+++ App.js
@@ -1,14 +1,7 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { NavigationContainer } from '@react-navigation/native';
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +11,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default function App() {
+  return (
+    <NavigationContainer>
+
+    </NavigationContainer>
+  );
+}
+
+
